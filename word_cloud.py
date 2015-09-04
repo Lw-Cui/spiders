01c4a493bffae8dd498dc88f3c91d1d96d71f637
Lw-Cui <645739460@QQ.com>
2015-09-04 23:02:29 +0800

    generate pic

---
+++ word_cloud.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*- 
+
+from os import path
+from chinese_cloud.chinese_cloud import ChineseCloud
+
+
+text_dir = path.dirname(__file__)
+text = open(path.join(text_dir, 'TopLabel.txt')).read()
+chinese_cloud = ChineseCloud(width=800, height=400, max_font=100, min_font=20).generate(text).to_image('TopLabel.png')
