e1e8e6dc4fdf5c6c9150db1583e9d9534fdd7cea
Lw-Cui <645739460@QQ.com>
2015-09-05 10:32:00 +0800

    add README

--- word_cloud.py
+++ word_cloud.py
@@ -7,4 +7,4 @@ from chinese_cloud.chinese_cloud import ChineseCloud
 
 text_dir = path.dirname(__file__)
 text = open(path.join(text_dir, 'TopLabel.txt')).read()
-chinese_cloud = ChineseCloud(width=800, height=400, max_font=100, min_font=20).generate(text).to_image('TopLabel.png')
+chinese_cloud = ChineseCloud(width=800, height=400, max_font=100, min_font=10).generate(text).to_image('TopLabel.png')
